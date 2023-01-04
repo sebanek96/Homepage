8015014b18688a4b70ab23610c6e2c33d3829c46
sebanek96 <116579697+sebanek96@users.noreply.github.com>
2023-01-04 17:25:30 +0100

    functions added

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
-console.log("Cześć!");
+{
+    const welcome = () => {
+        console.log("Welcome on my first webside");
+    }
 
-let przycisk = document.querySelector(".przycisk");
-let zdjecie = document.querySelector(".zdjecie");
+    const photoRemove = () => {
+        photo.remove();
+        button.remove();
 
-przycisk.addEventListener("click", () => {
-    zdjecie.remove()
-    przycisk.remove()
-});
+        const button = document.querySelector("removeButton");
+        const photo = document.querySelector(".photo");
+
+        button.addEventListener("click", photoRemove);
+        photoRemove();
+    };
+
+    welcome();
+    
+};
+
+
+
+
+
+
+
 
